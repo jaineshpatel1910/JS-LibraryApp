ddadf3c3377a4e24bccc4a06de629a44adb91676
jaineshpatel1910 <jaineshpatel.1910@gmail.com>
2022-07-09 08:34:33 +0530

    added the book records in table using JS

--- js/index.js
+++ js/index.js
@@ -1,1 +1,68 @@
-console.log("Library App")
+console.log("Library App");
+
+// constructor
+function Book(name, author, type) {
+    this.name = name;
+    this.author = author;
+    this.type = type;
+}
+
+// display constructor
+function Display() {
+
+}
+
+// add methods to display prototype
+Display.prototype.add = function (book) {
+    console.log("Adding to UI")
+    tableBody = document.getElementById("tableBody");
+    let uiString = `
+            <tr>
+                <td>${book.name}</td>
+                <td>${book.author}</td>
+                <td>${book.type}</td>
+            </tr> `;
+    tableBody.innerHTML += uiString;
+}
+
+// clear data from form after submit
+Display.prototype.clear = function () {
+    let libraryForm = document.getElementById("libraryForm");
+    libraryForm.reset();
+}
+
+
+// add submit event listener to libraryForm
+let libraryForm = document.getElementById("libraryForm");
+libraryForm.addEventListener("submit", libraryFormSubmit);
+
+function libraryFormSubmit(e) {
+    console.log("first")
+
+    let name = document.getElementById("bookName").value;
+    let author = document.getElementById("author").value;
+    let type;
+
+    let fiction = document.getElementById("fiction");
+    let programming = document.getElementById("programming");
+    let cooking = document.getElementById("cooking");
+
+    if (fiction.checked) {
+        type = fiction.value;
+    }
+    else if (programming.checked) {
+        type = programming.value;
+    }
+    else if (cooking.checked) {
+        type = cooking.value;
+    }
+
+    let book = new Book(name, author, type)
+    console.log(book, "book");
+
+    let display = new Display();
+    display.add(book);
+    display.clear();
+
+    e.preventDefault();
+}
